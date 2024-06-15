967b4bb97455f8e16814dc26098eef94f86d2da4
ryanleecode <drdgvhbh@gmail.com>
2024-06-15 10:33:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+node dist/esm/index.js
---
+++ dist/esm/index.js
@@ -0,0 +1,16 @@
+import { start } from 'smoldot';
+import { ksmcc3 } from '@substrate/connect-known-chains';
+import { createClient } from '@polkadot-api/substrate-client';
+import { getObservableClient } from '@polkadot-api/observable-client';
+import { getSmProvider } from '@polkadot-api/sm-provider';
+const smoldot = start();
+const chain = await smoldot.addChain({ chainSpec: ksmcc3 });
+const jsonRpcProvider = getSmProvider(chain);
+const client = getObservableClient(createClient(jsonRpcProvider));
+const chainhead = client.chainHead$();
+chainhead.bestBlocks$.subscribe({
+    next: (v) => console.log(v),
+    error: (err) => console.error(err),
+    complete: () => console.log("complete")
+});
+//# sourceMappingURL=index.js.map
